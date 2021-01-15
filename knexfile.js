1dfcaafe020edccbc83dcb19733e2ec77c9a9596
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2021-01-15 13:54:48 -0700

    update knexfile for production db

--- knexfile.js
+++ knexfile.js
@@ -21,18 +21,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DATABASE,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: DB_URL,
     migrations: {
       directory: __dirname + '/db/migrations',
     },
     seeds: {
       directory: __dirname + '/db/seeds',
     },
+    ssl: true,
   },
 };
